0463b4cc78527461e86de566f00374f897b5c42b
plarrat <larrat.philippe@lessonsharing.fr>
2022-02-03 11:03:43 +0100

    Gestion multi liste

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,50 @@
 import './App.css';
-import { Col, Container, Row} from 'react-bootstrap'
+import { Col, Container, Row, Button, InputGroup, FormControl} from 'react-bootstrap'
 
 import AppNavbar from './components/AppNavbar/AppNavbar';
 import TodoList from './components/TodoList/TodoList';
+import {useState} from 'react';
 
 function App() {
+  const [inputListe, setInputList] = useState()
+  const [liste, setListe] = useState([])
   
+  let displayListe = liste.map((item,indice)=>{
+    return (
+      <Col key={"liste-"+indice} md={6}>
+        <TodoList title={item} />
+      </Col>
+    )
+  })
+
+  function add(){
+    let tmp = [...liste];
+    tmp.push(inputListe)
+    setListe(tmp)
+    setInputList("")
+  }
+
   return (
     <div className="App">
-      <AppNavbar />
+      <AppNavbar  />
       <header className="App-header">
-        <Container>
+        <Container className="mt-5">
+          <Row>
+            <Col md={6}>
+              <InputGroup className="mb-3">
+                <FormControl
+                  placeholder="Création d'une liste" 
+                  value={inputListe}
+                  onChange={(e)=>setInputList(e.target.value)}
+                />
+                <Button variant="primary" onClick={add}>
+                  Ajouter une liste
+                </Button>
+              </InputGroup>
+            </Col>
+          </Row>
           <Row className="mt-4">
-            <Col md={6}>
-              <TodoList title="Mangas" />
-            </Col>
-
-            <Col md={6}>
-              <TodoList title="Animés" />
-            </Col>
-
-            <Col md={6}>
-              <TodoList title="Jeux PC" />
-            </Col>
-
-            <Col md={6}>
-              <TodoList title="Jeux Console" />
-            </Col>
-            
+            {displayListe}
           </Row>
         </Container>
       </header>
